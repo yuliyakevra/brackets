e5dad527c45682f983300853d3af1dbe2504ec6f
“Yulya <yulya_kevra@mail.ru>
2023-01-11 12:41:25 +0300

    feat: update index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let arr = [];
+
+    let pairs = {
+        ")" : "(",
+        "}": "{",
+        "]" : "[",
+            }
+    
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == "(" || str[i] == "{" || str[i] == "[") {
+            arr.push(str[i]);
+        }
+        else if (arr[arr.length - 1] == pairs[str[i]]) {
+            arr.pop()
+        }
+        else return false
+    }
+    return arr.length ? false : true
 }
